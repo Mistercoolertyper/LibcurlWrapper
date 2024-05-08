731002e75ec5aee1b4aeac8510617fbbea2447ce
Mistercoolertyper <84140706+Mistercoolertyper@users.noreply.github.com>
2024-05-08 10:17:03 -0700

    some error handling

--- Runner/Program.cs
+++ Runner/Program.cs
@@ -7,12 +7,24 @@ var curl = new Libcurl(new(
 	LibcurlConstants.CURL_TLS_VERSION.SSLVERSION_TLSv1_3
 ));
 
-var getResponse = curl.Get("https://cloudflare-quic.com/")!;
+var getResponse = curl.Get("https://cloudflare-quic.com/");
 
-Console.WriteLine($"GET Request: Http Version used: {getResponse.HttpVersion}; StatusCode: {getResponse.StatusCode}");
+if (getResponse != null)
+{
+	Console.WriteLine($"GET Request: Http Version used: {getResponse.HttpVersion}; StatusCode: {getResponse.StatusCode}");
+} else
+{
+	throw new Exception("Failed to make GET request!");
+}
 
 var postResponse = curl.Post("https://cloudflare-quic.com/", "{\"Some\": \"Data\"}", [
 	"Content-Type: application/json"
 ]);
 
-Console.WriteLine($"POST Request: Http Version used: {postResponse.HttpVersion}; StatusCode: {postResponse.StatusCode}");
+if (postResponse != null)
+{
+	Console.WriteLine($"POST Request: Http Version used: {postResponse.HttpVersion}; StatusCode: {postResponse.StatusCode}");
+} else
+{
+	throw new Exception("Failed to make POST Request!");
+}
